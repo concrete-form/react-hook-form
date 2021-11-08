1d40d809892f7d7413078847ecba8018fa1ebd66
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-08 09:33:30 -0500

    Refactored error string into translation objects

--- src/ReactHookFormHandler/ReactHookFormHandler.ts
+++ src/ReactHookFormHandler/ReactHookFormHandler.ts
@@ -1,7 +1,9 @@
 import { FormHandler } from '@concrete-form/core'
+import TranslationKeys from '@concrete-form/core/translation/TranslationKeys.enum'
 import { UseFormReturn } from 'react-hook-form'
 
 export default class ReactHookFormHandler implements FormHandler {
+  private cachedControlOptions: Record<string, any> = {}
   constructor (private readonly reactHookFormContext: UseFormReturn<any>) {
   }
 
@@ -14,6 +16,7 @@ export default class ReactHookFormHandler implements FormHandler {
   }
 
   public getControlProps (name: string, options?: any) {
+    this.cachedControlOptions[name] = options
     return this.reactHookFormContext.register(name, options)
   }
 
@@ -21,7 +24,7 @@ export default class ReactHookFormHandler implements FormHandler {
     const formState = this.reactHookFormContext.formState
     return {
       value: this.reactHookFormContext.getValues(name),
-      errors: this.formatFieldErrors(formState.errors[name]),
+      errors: this.formatFieldErrors(name, formState.errors[name]),
     }
   }
 
@@ -41,15 +44,41 @@ export default class ReactHookFormHandler implements FormHandler {
     )
   }
 
-  private formatFieldErrors (errors?: Record<string, any>) {
+  private formatFieldErrors (name: string, errors?: Record<string, any>) {
     if (!errors) {
       return []
     }
     const types = typeof errors?.types === 'object' ? errors.types : { [errors.type]: errors.message }
 
     return Object.entries(types).map(([errorType, message]) => {
-      // todo : use generic translations when no message is provided
-      return typeof message === 'string' && message !== '' ? message : errorType
+      if (message === true) {
+        return this.getGenericErrorMessage(errorType, this.cachedControlOptions[name] ?? {})
+      }
+      return typeof message === 'string' ? message : { key: TranslationKeys.DEFAULT, meta: { error: message } }
     }) || []
+  }
+
+  /**
+   * Translate React Hook Form built-in validations
+   */
+  private getGenericErrorMessage (type: string, options: Record<string, string|number>) {
+    switch (type) {
+      case 'required':
+        return { key: TranslationKeys.REQUIRED, meta: { type } }
+      case 'minLength':
+        return { key: TranslationKeys.STRING_MIN, values: { min: options.minLength }, meta: { type } }
+      case 'maxLength':
+        return { key: TranslationKeys.STRING_MAX, values: { max: options.maxLength }, meta: { type } }
+      case 'min':
+        return { key: TranslationKeys.NUMBER_MIN, values: { min: options.min }, meta: { type } }
+      case 'max':
+        return { key: TranslationKeys.NUMBER_MAX, values: { max: options.max }, meta: { type } }
+      case 'pattern':
+        return { key: TranslationKeys.DEFAULT, meta: { type } }
+      case 'validate':
+        return { key: TranslationKeys.DEFAULT, meta: { type } }
+      default:
+        return { key: TranslationKeys.DEFAULT, meta: { type } }
+    }
   }
 }
